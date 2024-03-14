03d5f8f9ebfc2d4787969bc5f593c8a047184b9b
Cathie <lynxluna12@gmail.com>
2024-03-14 21:52:58 +0200

    Added GUI functionality for system tray integration

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from __init__ import Logger, Analytics, Config, Donation
+import webbrowser
+import pystray
+from PIL import Image
+from __init__ import __version__, Logger, Analytics, Config, Donation
 
 # Class instantiation
 logger = Logger()
@@ -6,26 +9,50 @@ analytics = Analytics()
 config = Config()
 donation = Donation()
 
+def on_quit_clicked(icon, item):
+    if str(item) == "Start":
+        logger.log("Starting BoostEngine...")
+        config.load()
+        analytics.scan()
+
+        for process in analytics.running_processes:
+            if process not in config.config.get("whitelisted_processes", []):
+                logger.log(f"Killing process: {process}")
+                # Kill the process
+
+        for service in analytics.running_services:
+            if service not in config.config.get("whitelisted_services", []):
+                logger.log(f"Killing service: {service}")
+                # Kill the service
+
+        logger.log("BoostEngine finished.")
+
+    elif str(item) == "Pause":
+        logger.log("Paused BoostEngine...")
+        # Add pause functionality here
+        logger.log("BoostEngine paused.")
+
+    elif str(item) == "About":
+        # open url in web browser
+        logger.log("Opening donation page...")
+        webbrowser.open("https://github.com/Cathie02/BoostEngine")
+
+    elif str(item) == "Quit":
+        icon.stop()
 
 def main():
-    logger.log("Starting BoostEngine...")
-    config.load()
-    analytics.scan()
+    image = Image.open("icon.ico")
 
-    for process in analytics.running_processes:
-        if process not in config.config.get("whitelisted_processes", []):
-            logger.log(f"Killing process: {process}")
-            # Kill the process
-    
-    for service in analytics.running_services:
-        if service not in config.config.get("whitelisted_services", []):
-            logger.log(f"Killing service: {service}")
-            # Kill the service
+    # Create the tray icon
+    icon = pystray.Icon("BoostEngine Icon", image, menu=pystray.Menu(
+        pystray.MenuItem("Start", on_quit_clicked),
+        pystray.MenuItem("Pause", on_quit_clicked),
+        pystray.MenuItem("About", on_quit_clicked),
+        pystray.MenuItem("Quit", on_quit_clicked)
+    ), title=f"BoostEngine v{__version__}")
 
-    logger.log("BoostEngine finished.")
+    icon.run()
 
 
 if __name__ == "__main__":
     main()
-    donation.display()
-    
